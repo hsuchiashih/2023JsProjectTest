fb3dde9ddb89232e907c6fc9c5221ffe906f4e63
hsuchiashih <hsuchiashih@gmail.com>
2023-10-22 00:20:47 +0800

    test01

---
+++ myPage/all.js
@@ -0,0 +1,5 @@
+
+// axios.post('http://127.0.0.1:3000/posts', {title: '123'})
+// axios.delete('http://127.0.0.1:3000/posts/4');
+axios.get('http://127.0.0.1:3000/posts').then((res) => {console.log(res)})
+console.log('test');
